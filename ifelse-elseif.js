ce6b85b8ac4a6a045f8b684efb5b48e2cd7695db
Binish Farhan <binishfarhan89@gmail.com>
2023-09-25 15:26:40 +0500

    'done'

---
+++ ifelse-elseif.js
@@ -0,0 +1,54 @@
+// data types
+//1.string
+//2.integar
+//3.bolean
+
+var abc;
+// alert /// undefined
+// console.log
+// document.write
+// alert(abc);
+
+// abc = 'this is new variable'
+
+// alert(abc);
+
+// operator
+// + ,  add or concatenate
+// -
+// *
+// %
+// = assignment operator
+// == condition check krta but only value
+// === stricit checking hoti value and data type both are checked
+// !=  value check kre ga 
+// !== strict checking value + data type
+// >
+// <
+// &&
+// ||
+
+//  var obtMarks = prompt
+// var percentage = obtMarks/500 * 100
+
+if ( percentage >= 80){
+    alert('congratulation you got A+')
+}
+
+var num1 = +prompt('Enter any number' , '20');
+var num2 = 10
+var num = num1 * num2
+
+if(num <=  50){
+    alert('you are in if block')
+}
+else if(num > 50 && num <= 75){
+    alert('you are in 1st else if block')
+}
+else if(num > 75 && num <=100){
+    alert('you are in 2nd else if block')
+}
+else{
+    alert('program stop')
+}
+
